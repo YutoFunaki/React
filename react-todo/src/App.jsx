b580dfd8b34d8ae7275b09d655e70681d6a37d15
YutoFunaki <kaizuka0629@gmail.com>
2023-03-04 22:10:59 +0900

    todo基本の型

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,6 +1,46 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from "react";
+import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  const [incompleteTodo, setIncompleteTodo] = useState(["aaaa", "todo"]);
+  const [completeTodo, setCompleteTodo] = useState(["iiii", "complete"]);
+  return (
+  <>
+    <div className="input-area">
+      <input placeholder="todo"/>
+      <button>add</button>
+    </div>
+    <div className="incomplete-area">
+      <p className="title">未完了のtodo</p>
+      <ul>
+
+        {incompleteTodo.map((todo) => {
+          return(
+            <div key={todo} className="list-row">
+              <li>{todo}</li>
+              <button>完了</button>
+            <button>削除</button>
+          </div>
+          )
+        })}
+
+      </ul>
+    </div>
+    <div className="complete-area">
+      <p className="title">完了したtodo</p>
+        <ul>
+
+          {completeTodo.map((todo) => {
+            return(
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            )
+          })}
+
+        </ul>
+    </div>
+  </>
+  );
 };
