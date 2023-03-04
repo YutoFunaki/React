d945bb8d1e4bc8ffeac592877ff1647e665b4178
YutoFunaki <kaizuka0629@gmail.com>
2023-03-04 16:21:33 +0900

    動的な設定をするならuseStateを使う

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const OnClickButton = () => alert();
+  const OnClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const [num, setNum] = useState(0);
   return (
     <>
      <h1 style={{ color: 'red' }}>hello!Im YutoFunaki!</h1>
      <ColorfulMessage color="yellow">How are you?</ColorfulMessage>
      <ColorfulMessage color="gray">Im good.</ColorfulMessage>
-     <button onClick={OnClickButton}>ボタン</button>
+     <button onClick={OnClickCountUp}>ボタン</button>
+     <p>{num}</p>
     </>
     );
 };
